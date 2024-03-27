f45e0f968a3573e04236e3c4e20fd6734a4ed2ac
Prachi <saini.prachi1995@gmail.com>
2024-03-27 16:40:45 +0530

    update detail & add screenshot

--- src/Selected Pokemon/SelectedPokemon.jsx
+++ src/Selected Pokemon/SelectedPokemon.jsx
@@ -1,24 +1,40 @@
 import Stats from "../Stats/Stats";
+import "./SelectedPokemon.css";
 
 function SelectedPokemon({ selectedPokemon }) {
-    return (
-        <div className="pokemon-details">
-            <h2>{selectedPokemon.name.toUpperCase()}</h2>
-            <img src={selectedPokemon.sprites.other.home.front_default} alt={selectedPokemon.name} />
-            <div><h3>Types:</h3> {selectedPokemon.types.map(type => type.type.name).join(', ')}</div>
-            <div className="moves-section">
-                <h3>Moves:</h3>
-                <ul className="move-list">
-                    {selectedPokemon.moves.slice(0, 8).map((move, index) => (
-                        <li key={index} className="move-item">
-                            <span className="move-name">{move.move.name}</span> - <span className="move-level-learned">Learned at level {move.version_group_details[0].level_learned_at}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <Stats name={selectedPokemon.name} />
+  return (
+    <div className="pokemon-details-card">
+      <h1 className="pokemon-title">{selectedPokemon.name.toUpperCase()}</h1>
+      <div>
+        <img
+          className="pokemon-img"
+          src={selectedPokemon.sprites.other.home.front_default}
+          alt={selectedPokemon.name}
+        />
+        <div className="pokemon-info">
+          <div>
+            <h3>Types:</h3>{" "}
+            {selectedPokemon.types.map((type) => type.type.name).join(", ")}
+          </div>
+          <div className="moves-section">
+            <h3>Moves:</h3>
+            <ul className="move-list">
+              {selectedPokemon.moves.slice(0, 8).map((move, index) => (
+                <li key={index} className="move-item">
+                  <span className="move-name">{move.move.name}</span> -{" "}
+                  <span className="move-level-learned">
+                    Learned at level{" "}
+                    {move.version_group_details[0].level_learned_at}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+      <Stats name={selectedPokemon.name} />
+    </div>
+  );
 }
 
 export default SelectedPokemon;
